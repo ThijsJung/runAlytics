137d75cc44421df981cb218238ef96e367529f30
Thijs Jung <thijs.jung@trivago.com>
2017-07-02 22:19:24 +0200

    Adding handlers and api code

---
+++ src/api.py
@@ -0,0 +1,33 @@
+import decimal
+import os
+import json
+
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(os.environ['RUNS_TABLE'])
+
+class DecimalEncoder(json.JSONEncoder):
+    """Helper class to convert a DynamoDB item to JSON."""
+
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if o % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+
+def get_runs(event, context):
+    db_runs = table.scan(ProjectionExpression='run_id')
+    runs = list()
+    if db_runs['Items']:
+        runs = [run['run_id'] for run in db_runs['Items']]
+    body = json.dumps(runs)
+    return dict(statusCode=200, body=body)
+
+def get_run_data(event, context):
+    run_id = str(event['pathParameters']['run_id'])
+    run_data = table.get_item(Key={'run_id': run_id})
+    body = json.dumps(run_data, cls=DecimalEncoder)
+
+    return dict(statusCode=200, body=body)
---
+++ src/handlers.py
@@ -0,0 +1,16 @@
+import os
+import boto3
+
+from fit_parser import Uploader
+
+s3 = boto3.client('s3')
+
+
+def handler(event, context):
+    uploader = Uploader()
+    if 'Records' in event:      # event is triggered by S3
+        for record in event['Records']:
+            bucket = record['s3']['bucket']['name']
+            key = record['s3']['object']['key']
+            fit_file = s3.get_object(Bucket=bucket, Key=key)['Body'].read()
+            uploader.run(fit_file)
