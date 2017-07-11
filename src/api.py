30a348815a5d73f75ceba92bbb3d4309f93030a7
Thijs Jung <thijs.jung@trivago.com>
2017-07-11 07:49:55 +0200

    Fixing javascript CORS error

--- src/api.py
+++ src/api.py
@@ -23,11 +23,11 @@ def get_runs(event, context):
     if db_runs['Items']:
         runs = [run['run_id'] for run in db_runs['Items']]
     body = json.dumps(runs)
-    return dict(statusCode=200, body=body)
+    return dict(statusCode=200, body=body, headers={'Access-Control-Allow-Origin': '*'})
 
 def get_run_data(event, context):
     run_id = str(event['pathParameters']['run_id'])
     run_data = table.get_item(Key={'run_id': run_id})
     body = json.dumps(run_data, cls=DecimalEncoder)
 
-    return dict(statusCode=200, body=body)
+    return dict(statusCode=200, body=body, headers={'Access-Control-Allow-Origin': '*'})
