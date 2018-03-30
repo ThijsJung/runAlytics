3ee9321da9df1fbac11dff0d2f476b53ca73d325
Thijs Jung <thijs.jung@gmail.com>
2018-03-30 18:08:45 +0200

    Implemented FITManager to get and save FIT files to S3

--- src/runs/managers.py
+++ src/runs/managers.py
@@ -1,9 +1,33 @@
 import json
+from tempfile import NamedTemporaryFile
 
 import boto3
 
 from runs.models import Run
 from utils import DecimalEncoder
+
+class FITManager():
+    prefix = 'FIT_files'
+
+    def __init__(self, s3_client):
+        self.s3_client = s3_client
+    
+    def _generate_key(self, fit_id):
+        return f"{self.prefix}/{fit_id}"
+    
+    def save(self, fit_file):
+        pass
+    
+    def get(self, record: dict) -> bytes:
+        bucket = record['s3']['bucket']['name']
+        key = record['s3']['object']['key']
+
+        fit_file = self.s3_client.get_object(
+            Bucket=bucket,
+            Key=self._generate_key(key)
+        )['Body'].read()
+
+        return fit_file
 
 
 class RunsManager():
--- tests/test_runs/test_managers.py
+++ tests/test_runs/test_managers.py
@@ -1,42 +1,57 @@
 from unittest.mock import Mock
 
 import boto3
+import pytest
 
 from runs.managers import RunsManager
 from runs.models import Run
 
 mock_dynamodb_table = Mock(spec=boto3.resource(
     'dynamodb').Table('test-table-name'))
+mock_s3_resource = Mock(spec=boto3.client('s3'))
+
+class TestRunsManager:
+    def test_save(self):
+        manager = RunsManager(mock_dynamodb_table)
+        run = Run(id='test_id')
+
+        manager.save(run)
+
+        mock_dynamodb_table.put_item.assert_called_with(
+            Item='{"id": "test_id", "created_at": null, "data": {"distances": [], "heart_rates": [], "coordinates": [], "timestamps": []}}'
+        )
 
 
-def test_manager_save():
-    manager = RunsManager(mock_dynamodb_table)
-    run = Run(id='test_id')
+    def test_delete(self):
+        manager = RunsManager(mock_dynamodb_table)
 
-    manager.save(run)
+        manager.delete('test_id')
 
-    mock_dynamodb_table.put_item.assert_called_with(
-        Item='{"id": "test_id", "created_at": null, "data": {"distances": [], "heart_rates": [], "coordinates": [], "timestamps": []}}'
-    )
-
-
-def test_manager_delete():
-    manager = RunsManager(mock_dynamodb_table)
-
-    manager.delete('test_id')
-
-    mock_dynamodb_table.delete_item.assert_called_once_with(
-        Key='test_id'
-    )
+        mock_dynamodb_table.delete_item.assert_called_once_with(
+            Key='test_id'
+        )
 
 
-def test_manager_get():
-    manager = RunsManager(mock_dynamodb_table)
-    mock_dynamodb_table.get_item.return_value = {'id': 'test_id'}
+    def test_get(self):
+        manager = RunsManager(mock_dynamodb_table)
+        mock_dynamodb_table.get_item.return_value = {'id': 'test_id'}
 
-    run = manager.get('test_id')
+        run = manager.get('test_id')
 
-    mock_dynamodb_table.get_item.assert_called_with(
-        Key='test_id'
-    )
-    assert run == Run('test_id')
+        mock_dynamodb_table.get_item.assert_called_with(
+            Key='test_id'
+        )
+        assert run == Run('test_id')
+
+class TestFITManager:
+    @pytest.mark.skip(reason="Must write tests")
+    def test__generate_key(self):
+        pass
+    
+    @pytest.mark.skip(reason="Must write tests")
+    def test_save(self):
+        pass
+    
+    @pytest.mark.skip(reason="Must write tests")
+    def test_get(self, key):
+        pass
